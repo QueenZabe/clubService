ebdd45b0a94f026361453c94a1d27fab0b1f5f11
Ljh081021 <loveljh081021@dgsw.hs.kr>
2025-11-24 20:57:15 +0900

    feat :: delete club

--- src/main/java/com/example/clubservice/presentation/ClubController.java
+++ src/main/java/com/example/clubservice/presentation/ClubController.java
@@ -1,7 +1,22 @@
 package com.example.clubservice.presentation;
 
+import com.example.clubservice.service.ClubService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/clubs")
+@AllArgsConstructor
 public class ClubController {
+
+    private final ClubService clubService;
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteClub(@PathVariable("id") Long id) {
+        return clubService.deleteClub(id);
+    }
 }
--- src/main/java/com/example/clubservice/presentation/dto/res/ClubListRes.java
+++
@@ -1,4 +0,0 @@
-package com.example.clubservice.presentation.dto.res;
-
-public class ClubListRes {
-}
