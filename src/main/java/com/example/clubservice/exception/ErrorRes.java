f9cb6f81272bceb168e393881c3b93b050f9b87a
Ljh081021 <165773350+Ljh081021@users.noreply.github.com>
2025-11-26 21:38:00 +0900

    name

--- src/main/java/com/example/clubservice/exception/ErrorRes.java
+++ src/main/java/com/example/clubservice/exception/ErrorRes.java
@@ -7,7 +7,7 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @Builder
 @RequiredArgsConstructor
-public class ErrorRes {
+public class ErrorResponse {
     private final int status;
     private final String message;
 }
